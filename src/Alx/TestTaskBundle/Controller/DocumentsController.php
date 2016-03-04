5af721a04ca0a3dfa4b0d1b308647a6b13a8cac6
alexey <alxant7@gmail.com>
2016-03-04 15:45:00 +0400

    Documents is ready

--- src/Alx/TestTaskBundle/Controller/DocumentsController.php
+++ src/Alx/TestTaskBundle/Controller/DocumentsController.php
@@ -45,23 +45,13 @@ class DocumentsController extends Controller
             $em->persist($document);
             $em->flush();
 
-            return $this->redirectToRoute('documents_show', ['id' => $document->getId()]);
+            $this->addFlash('success', 'Document has been successfully created');
+
+            return $this->redirectToRoute('documents_index');
         }
 
         return $this->render('@AlxTestTask/documents/new.html.twig', [
             'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * Finds and displays a Document entity.
-     * @param Document $document
-     * @return Response
-     */
-    public function showAction(Document $document)
-    {
-        return $this->render('@AlxTestTask/documents/show.html.twig', [
-            'document' => $document
         ]);
     }
 
@@ -73,19 +63,21 @@ class DocumentsController extends Controller
      */
     public function editAction(Request $request, Document $document)
     {
-        $editForm = $this->createForm(DocumentType::class, $document, ['method' => 'PUT']);
-        $editForm->handleRequest($request);
+        $form = $this->createForm(DocumentType::class, $document, ['method' => 'PUT']);
+        $form->handleRequest($request);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($document);
             $em->flush();
 
-            return $this->redirectToRoute('documents_show', ['id' => $document->getId()]);
+            $this->addFlash('success', 'Document has been successfully modified');
+
+            return $this->redirectToRoute('documents_index');
         }
 
         return $this->render('@AlxTestTask/documents/edit.html.twig', [
-            'edit_form' => $editForm->createView()
+            'form' => $form->createView()
         ]);
     }
 
@@ -104,6 +96,8 @@ class DocumentsController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->remove($document);
             $em->flush();
+
+            $this->addFlash('success', 'Document has been successfully removed');
         }
 
         return $this->redirectToRoute('documents_index');
